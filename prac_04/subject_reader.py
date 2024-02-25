f3c6ea62db4258cc6fc41c0189e07aff1e7d2be0
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-02-25 22:31:57 +1000

    Converting Data Strings to Lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 Practical
+CP1404 - prac04
 Data file -> lists program
 """
 
@@ -8,22 +8,26 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []  # Initialize an empty list to store the data
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subject_data.append(parts)  # Append the parts to the subject_data list
     input_file.close()
+    return subject_data  # Return the list of lists
+
+
+def display_subject_details(data):
+    """Display subject details."""
+    for subject in data:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 main()
