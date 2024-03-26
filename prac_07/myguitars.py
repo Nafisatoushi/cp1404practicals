f6a9538712113986292d896ac8f9388c7c0581be
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-03-27 00:35:48 +1000

    Prompt the user to enter the details of their new guitars.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -26,3 +26,37 @@ def save_guitars_to_file(guitars, file_name):
             file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
 
+def get_new_guitar():
+    """Get details of a new guitar from the user."""
+    name = input("Enter the name of the guitar: ")
+    year = int(input("Enter the year the guitar was made: "))
+    cost = float(input("Enter the cost of the guitar: $"))
+    return Guitar(name, year, cost)
+
+
+def main():
+    guitars = load_guitars_from_file('guitars.csv')
+
+    # Display existing guitars
+    print("Existing Guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+    # Get details of new guitars from the user
+    num_new_guitars = int(input("How many new guitars do you want to add? "))
+    for i in range(num_new_guitars):
+        print(f"\nEntering details for Guitar #{i + 1}:")
+        new_guitar = get_new_guitar()
+        guitars.append(new_guitar)
+
+    # Save guitars to file
+    save_guitars_to_file(guitars, 'guitars.csv')
+    print("\nNew guitars saved to file.")
+
+    # Display all guitars after adding new ones
+    print("\nAll Guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
