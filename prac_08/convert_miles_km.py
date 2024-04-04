584363f6aa6b28fb193ed30036d9965f491549f4
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-04-04 20:29:27 +1000

    Use a StringProperty for the text on the output (km) label.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,3 +3,9 @@ actual time: """
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    output_text = StringProperty("")
