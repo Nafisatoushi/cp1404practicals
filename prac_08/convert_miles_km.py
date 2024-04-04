c771ade8027ce930ce493888a8a146153067f627
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-04-04 20:31:59 +1000

    Create a function to allow typing a number in the text entry field.

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -9,3 +9,10 @@ MILES_TO_KM = 1.60934
 class MilesConverterApp(App):
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
     output_text = StringProperty("")
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_m_km_solution.kv')
+        self.output_text = "Type in the field & press Enter"
+        return self.root
