1439eba34c54ba0a734e92c314c6f94dc8235224
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-04-15 01:36:16 +1000

    Add instantiation of UnreliableCar objects.

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,6 @@
+from prac_09.unreliable_car import UnreliableCar
+
+# Create an unreliable car with name, fuel, and reliability
+my_car = UnreliableCar("Mostly Good", 100, 90)
+my_dodgy_car = UnreliableCar("Dodgy", 100, 9)
+
