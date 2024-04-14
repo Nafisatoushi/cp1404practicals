dda9bfccbbc2de4a8939aac7e77f7388b5628609
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-04-15 01:26:30 +1000

    Implement driving functionality with reliability check.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,11 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car if reliability is met and return distance driven."""
+        if random.randint(0, 100) < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
