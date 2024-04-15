ce907abf5828da4f21297c2e8b0d3718a9f08eb3
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-04-15 23:07:19 +1000

    Handle Wikipedia disambiguation error

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,3 +4,11 @@ while not False:
     search_phrase = input("Enter a page title or search phrase (or blank to quit): ")
     if not search_phrase:
         break
+
+    try:
+        page = wikipedia.page(search_phrase)
+        print("Title:", page.title)
+        print("Summary:", page.summary)
+        print("URL:", page.url)
+    except wikipedia.exceptions.DisambiguationError as e:
+        pass  # Handle disambiguation error here
