ed2ee65afea7e85256388f0e7d34a10d69545a93
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-06 22:51:23 +1000

    do the question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,3 +11,12 @@ in_file = open("name.txt", "r")
 name = in_file.read().strip()
 print(f"Hi {name}!")
 in_file.close()
+
+# Task 3 - Sum of the first two numbers
+with open("numbers.txt", "r") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print("The sum of the first two numbers is:", number1 + number2)
+
+
+
