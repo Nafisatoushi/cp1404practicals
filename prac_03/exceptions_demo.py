6fddfe86aefe40e5eda475ac77bb34959a1ef42b
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-06 21:39:09 +1000

    3 answers

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -5,6 +5,11 @@ Answer the following questions:
 2. When will a ZeroDivisionError occur?
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
+# Answer 1:  ValueError will occur if the user enters a non-integer for numerator or denominator.
+
+# Answer 2:ZeroDivisionError will occur if the user enters 0 for the denominator.
+
+# Answer 3:Yes, we can change the code to check if the denominator is zero before performing the division.
 
 try:
     numerator = int(input("Enter the numerator: "))
