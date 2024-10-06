13395c71d7cd8f6ad248f94dd4aa78d495dde650
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-06 22:46:23 +1000

    do the question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,3 +5,9 @@ out_file = open("name.txt", "w")
 name = input("What is your name? ")
 print(name, file=out_file)  # or out_file.write(name)
 out_file.close()
+
+# Task 2
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+print(f"Hi {name}!")
+in_file.close()
