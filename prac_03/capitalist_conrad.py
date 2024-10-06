063bb3c78fe67354570f34f770d341b2338f4b99
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-06 21:34:36 +1000

    calculate the price

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -11,8 +11,8 @@ import random
 
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05   # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 OUTPUT_FILE = "stock_price_simulation.txt"
 
@@ -22,10 +22,11 @@ number_of_days = 0
 # Open the file for writing
 out_file = open(OUTPUT_FILE, 'w')
 
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -38,6 +39,13 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}", file=out_file)
+    number_of_days += 1
+
+    # Print to console
+    print(f"On day {number_of_days} price is: ${price:,.2f}")
+
+    # Print to file
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
 # Close the file
 out_file.close()
