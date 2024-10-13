77ab706f89c81a06781d515018bf260e0d4d4aea
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-13 19:57:39 +1000

    get the main function

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,14 @@
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("That makes no sense!")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    
