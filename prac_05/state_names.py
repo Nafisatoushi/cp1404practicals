bbdd70517b2de2dbc329e681697839e529bb0843
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-20 19:44:31 +1000

    print all the states and names neatly

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,10 +4,13 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+# Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+# Print all states and names
+for code, name in CODE_TO_NAME.items():
+    print(f"{code} is {name}")
+
 
 state_code = input("Enter short state: ")
 while state_code != "":
