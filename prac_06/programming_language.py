f79bf72822a26b9ab2348794695b06ffbd63693a
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-31 18:18:08 +1000

    write the __str__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,7 +4,7 @@ Estimate: 30 minutes
 Actual:   40 minutes
 """
 
-class ProgrammingLanguage:class ProgrammingLanguage:
+class ProgrammingLanguage:
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -14,5 +14,8 @@ class ProgrammingLanguage:class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing.lower() == 'dynamic'
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
 
