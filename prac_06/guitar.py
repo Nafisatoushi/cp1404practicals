226fd370a3ed741541b3d289dce33f6309e384b9
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-31 18:27:15 +1000

    define the init and str method

---
+++ prac_06/guitar.py
@@ -0,0 +1,10 @@
+class Guitar:
+    """Represent a Guitar object."""
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
