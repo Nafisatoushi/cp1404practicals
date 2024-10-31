b9573c2112b9035a3539b0d1ba301936e6d50ce4
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-10-31 18:18:35 +1000

    Loop through and print the names

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,5 +1,27 @@
 from programming_language import ProgrammingLanguage
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    # print language details
+    print(python)
+    print(ruby)
+    print(visual_basic)
+
+
+    # list containing languages
+    languages = [python, ruby, visual_basic]
+
+    print("\nThe dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
+
+
+
+
