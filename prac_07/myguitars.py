23f0a198910531413da467c1164a6065ba5aa6a3
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-11-10 21:58:05 +1000

    get new guitars and save them

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -17,6 +17,20 @@ def load_guitars_from_file(file_name):
         print("File not found. Creating a new file...")
     return guitars
 
+
+def get_new_guitar():
+    """Get details of a new guitar from the user."""
+    name = input("Enter the name of the guitar: ")
+    year = int(input("Enter the year the guitar was made: "))
+    cost = float(input("Enter the cost of the guitar: $"))
+    return Guitar(name, year, cost)
+
+def save_guitars_to_file(guitars, file_name):
+    """Save guitars to a file."""
+    with open(file_name, 'w') as file:
+        for guitar in guitars:
+            file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+
 def main():
     guitars = load_guitars_from_file('guitars.csv')
     guitars.sort()
@@ -25,5 +39,6 @@ def main():
     print("Existing Guitars:")
     for guitar in guitars:
         print(guitar)
+
 
 main()
