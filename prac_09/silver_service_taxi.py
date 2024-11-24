d9a7f607bc1489fe4d9012ede143a63bff232c47
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-11-24 22:41:31 +1000

    set flagfall to 4.50

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,7 +3,7 @@ from prac_09.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     """A Silver Service Taxi, which is a specialized version of a Taxi."""
 
-
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
         """Initialize a SilverServiceTaxi instance, based on parent class Taxi."""
