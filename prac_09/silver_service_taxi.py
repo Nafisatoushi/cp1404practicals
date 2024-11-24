50cf1b230f89ed522d21cd5c54236ee8c84875d0
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-11-24 23:01:12 +1000

    fix it

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,6 +8,7 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel, fanciness):
         """Initialize a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
+        self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def get_fare(self):
