19c26b78c7fa5935e59399d30dd9d53dd132c5f0
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-11-25 11:01:26 +1000

    get default string value and taxi choices

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,3 +2,11 @@ from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None  #default starting value
+    print("Let's drive!")
+    print(MENU)
