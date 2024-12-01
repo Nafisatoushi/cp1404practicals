ccf113f20b769af8df2d69bcbc9af8b53a5884a3
nafisatoushi <nafisasadaf.toushi@my.jcu.edu.au>
2024-12-01 23:20:43 +1000

    write a program that prompts the user for a page title and search phrase

---
+++ prac_10/wiki.py
@@ -0,0 +1,6 @@
+import wikipedia
+def main():
+    """Prompt the user for a page title or search phrase and display details."""
+    print("Welcome to the Wikipedia search tool!")
+
+    search_phrase = input("Enter page title (or press Enter to exit): ")
